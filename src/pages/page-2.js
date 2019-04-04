0565168c3d48d7d36f205e221b28bc03dd063634
Dev05 <dev05@npdcdev.local>
2019-04-04 16:40:13 -0300

    Initial Commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,11 +4,29 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components'
+
+const PrimaryButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-family: 'Roboto', 'Merriweather', sans-serif;
+  font-weight: 100;
+`
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <PrimaryButton>Normal Button</PrimaryButton>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,35 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
+  googleFonts: [
+    {
+      name: 'Roboto',
+      styles: [
+        '100',
+        '400'
+      ],
+    },
+    {
+      name: 'Merriweather',
+      styles: [
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+})
+
+export default typography
